9cd53e87af4c7a973aa8d470aafc429844b7ad72
LAPTOP-QA62HH1U\saimo <saimohanmajoju@gmail.com>
2023-06-23 15:02:41 -0400

    Application with InMemeory UserDetails Manager used for demo appliations

--- KeyCloakSSO/src/main/java/com/Keycloak/KeyCloakSSO/Config/SecurityConfig.java
+++ KeyCloakSSO/src/main/java/com/Keycloak/KeyCloakSSO/Config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.Keycloak.KeyCloakSSO.Config;
 
+import com.Keycloak.KeyCloakSSO.Controllers.NoticesController;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -35,5 +41,25 @@ public class SecurityConfig {
 //
 //        return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsServices(){
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
 }
